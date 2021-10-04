953497a7ad05f6e853bfab55cdd8c9342630a6e2
Lacrimosa99 <lacrimosa99@gmx.de>
2021-10-04 22:14:00 +0200

    Update easy-wi_install.sh

--- easy-wi_install.sh
+++ easy-wi_install.sh
@@ -1215,9 +1215,9 @@ _EOF_
 			echo '[mysqld]' >/etc/mysql/conf.d/disable_strict_mode.cnf
 			if [ "$MYSQL_VERSION" -lt "80" ]; then
 				echo 'sql_mode=IGNORE_SPACE,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' >>/etc/mysql/conf.d/disable_strict_mode.cnf
-      else
-        echo 'sql_mode=IGNORE_SPACE,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' >>/etc/mysql/conf.d/disable_strict_mode.cnf
-      fi
+			else
+				echo 'sql_mode=IGNORE_SPACE,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' >>/etc/mysql/conf.d/disable_strict_mode.cnf
+			fi
 		fi
 	fi
 
